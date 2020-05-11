320d0031499060e611ce73126e8c627b6b58e438
Ravinder Sandhu <RS076@atcltd.atc.org.nz>
2020-05-12 00:22:49 +1200

    Add project files.

---
+++ FirstNameLastName/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FirstNameLastName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is Name?");
+            Console.Write("your first name: ");
+            String FirstName;
+            FirstName = Console.ReadLine();
+
+            string LastName;
+            Console.Write("your last name");
+            LastName = Console.ReadLine();
+            Console.WriteLine("Hi " + FirstName + " " + LastName);
+            Console.ReadLine();
+
+        }
+    }
+}
